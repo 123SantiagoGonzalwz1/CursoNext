a9fc7bf4350fb1fe41b5bbe9b74a4a6d24879c60
Santiago González <oscarsantiagogonzalezforero@gmail.com>
2024-09-25 12:16:01 -0500

    Navegación

--- next-js-prisma-crud/src/app/about/page.jsx
+++ next-js-prisma-crud/src/app/about/page.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
 
 function AboutPage() {
   return (
-    <div>
-      AboutPage
-    </div>
-  )
+    <section className="container mx-auto">
+      <h3 className="text-4xl font-bold my-5">About</h3>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi vero
+        sint qui blanditiis, similique alias incidunt saepe magnam dicta,
+        maiores nulla quisquam dignissimos perferendis quia numquam quibusdam
+        fuga, maxime aspernatur ipsam facilis nisi non commodi exercitationem
+        expedita. Obcaecati nostrum nobis amet neque, tempora eos fuga optio
+        ipsa similique rem sapiente, praesentium quam provident itaque quidem ex
+        quas pariatur nulla necessitatibus impedit quod fugit in? Rem quod,
+        ducimus doloribus ab atque aperiam accusantium, aspernatur reiciendis
+        consequatur et numquam, vel qui distinctio? Voluptatibus laboriosam ab
+        accusantium suscipit perferendis illo commodi dolor, eaque natus, sunt
+        quae earum maxime mollitia iste modi explicabo placeat.
+      </p>
+    </section>
+  );
 }
 
-export default AboutPage
+export default AboutPage;
