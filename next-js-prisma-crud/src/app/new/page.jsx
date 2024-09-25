3f5a9f63b25af87588109c6ace59d3cfb8be60a2
Santiago González <oscarsantiagogonzalezforero@gmail.com>
2024-09-25 11:49:03 -0500

    Funcionamiento de eliminación

--- next-js-prisma-crud/src/app/new/page.jsx
+++ next-js-prisma-crud/src/app/new/page.jsx
@@ -1,58 +1,58 @@
-'use client';
+"use client";
 
 import { useRouter } from "next/navigation";
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 function NewPage({ params }) {
-
   const router = useRouter();
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
 
   useEffect(() => {
-    if(params.id) {
+    if (params.id) {
       fetch(`/api/tasks/${params.id}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setTitle(data.title)
-        setDescription(data.description)
-      });
+        .then((res) => res.json())
+        .then((data) => {
+          setTitle(data.title);
+          setDescription(data.description);
+        });
     }
   }, []);
 
   const onSubmit = async (e) => {
     e.preventDefault();
 
-    if(params.id) {
+    if (params.id) {
       const res = await fetch(`/api/tasks/${params.id}`, {
-        method: 'PUT',
+        method: "PUT",
         body: JSON.stringify({ title, description }),
         headers: {
-          'Content-type': 'application/json'
+          "Content-type": "application/json",
         },
       });
 
       const data = await res.json();
       console.log(data);
-
     } else {
-      const res = await fetch('/api/tasks', {
-        method: 'POST',
+      const res = await fetch("/api/tasks", {
+        method: "POST",
         body: JSON.stringify({ title, description }),
         headers: {
-          'Content-type': 'application/json'
-        }
+          "Content-type": "application/json",
+        },
       });
       const data = await res.json();
     }
-    
+
     router.refresh();
     router.push("/");
-  }
+  };
   return (
     <div className="h-screen flex justify-center items-center">
       <form className="bg-slate-800 p-10 w-1/4" onSubmit={onSubmit}>
-        <label htmlFor="title" className="font-bold text-sm">Título de la tarea</label>
+        <label htmlFor="title" className="font-bold text-sm">
+          Título de la tarea
+        </label>
         <input
           type="text"
           id="title"
@@ -61,7 +61,9 @@ function NewPage({ params }) {
           onChange={(e) => setTitle(e.target.value)}
           value={title}
         />
-        <label htmlFor="description" className="font-bold text-sm">Descripción de la tarea</label>
+        <label htmlFor="description" className="font-bold text-sm">
+          Descripción de la tarea
+        </label>
         <textarea
           id="description"
           rows="3"
@@ -70,9 +72,30 @@ function NewPage({ params }) {
           onChange={(e) => setDescription(e.target.value)}
           value={description}
         ></textarea>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Crear
-        </button>
+        <div className="flex justify-between">
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Crear
+          </button>
+          {params.id && (
+            <button
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+              type="button"
+              onClick={async () => {
+                const res = await fetch(`/api/tasks/${params.id}`, {
+                  method: "DELETE",
+                });
+                const data = await res.json();
+                router.refresh();
+                router.push("/");
+              }}
+            >
+              Eliminar
+            </button>
+          )}
+        </div>
       </form>
     </div>
   );
