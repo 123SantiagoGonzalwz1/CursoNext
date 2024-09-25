688a553a4c97406542f184119192f7788ebf7c99
Santiago González <oscarsantiagogonzalezforero@gmail.com>
2024-09-25 10:04:58 -0500

    Listando tareas

--- next-js-prisma-crud/src/app/page.jsx
+++ next-js-prisma-crud/src/app/page.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { prisma } from "@/libs/prisma";
+import TaskCard from "@/components/TaskCard";
 
-function HomePage() {
-  return (
-    <div>
-      Tareas
-    </div>
-  )
+async function loadTasks() {
+  return await prisma.tasks.findMany();
 }
 
-export default HomePage
+async function HomePage() {
+  const tasks = await loadTasks();
+
+  return (
+    <section className="container mx-auto">
+      <div className="grid grid-cols-3 gap-3 mt-10">
+        {tasks.map((task) => (
+          <TaskCard task={task} key={task.id}/>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default HomePage;
